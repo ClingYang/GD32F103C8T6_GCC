8b479179a1e7556c0a0476e0b535e3e00713e49f
Cling <yangsonglin0813@foxmail.com>
2024-10-24 00:59:11 +0800

    为什么我的空闲中断实现不了串口2的

--- Bsp/usart.c
+++ Bsp/usart.c
@@ -65,12 +65,4 @@ int _write(int fd, char *pBuffer, int size)
     return size;
 }
 
-// /* retarget the C library printf function to the USART */
-// int fputc(int ch, FILE *f)
-// {
-//     usart_data_transmit(USART0, (uint8_t)ch);
-//     while (RESET == usart_flag_get(USART0, USART_FLAG_TBE))
-//         ;
-//     return ch;
-// }
 
