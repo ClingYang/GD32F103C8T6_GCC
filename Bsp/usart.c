bbd780e17bb1641c6fd69ef86fe8abfefb437428
ClingYang <yangsonglin0813@foxmail.com>
2024-10-24 14:05:32 +0800

    添加兼容问题的代码段GNUC环境

--- Bsp/usart.c
+++ Bsp/usart.c
@@ -54,6 +54,7 @@ void nvic_config(void)
 }
 
 
+#ifdef __GNUC__
 int _write(int fd, char *pBuffer, int size)
 {
     for (int i = 0; i < size; i++)
@@ -64,7 +65,16 @@ int _write(int fd, char *pBuffer, int size)
     }
     return size;
 }
-
+#else
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART2, (uint8_t)ch);
+    while (RESET == usart_flag_get(USART2, USART_FLAG_TBE))
+        ;
+    return ch;
+}
+#endif
 void USART2_IRQHandler(void)
 {
     if(RESET != usart_interrupt_flag_get(USART2, USART_INT_FLAG_IDLE)) {
